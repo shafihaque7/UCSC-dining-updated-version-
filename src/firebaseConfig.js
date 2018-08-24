96936f6c4c33f8f2ea82dad107367232e545f154
Shafi Haque <shafihaque7@gmail.com>
2018-08-24 17:45:14 -0400

    Delete firebaseConfig.js

--- src/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-   apiKey: "AIzaSyBID-Y48V9vHmFyaVIZN6KoYN5PC4tJYrY",
-   authDomain: "ucscdining2.firebaseapp.com",
-   databaseURL: "https://ucscdining2.firebaseio.com",
-   projectId: "ucscdining2",
-   storageBucket: "ucscdining2.appspot.com",
-   messagingSenderId: "1045767642427"
-}
